7918a8f95ef97c2975546a1f24351e4833730c63
smatand <andrejsmatana0@gmail.com>
2024-01-28 22:34:52 +0100

    Rewrite mongodb from sync to async

--- vinted/mongodb.py
+++ vinted/mongodb.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient
+import motor.motor_asyncio as motor
 from settings import MONGODB_CONNSTRING
 
 COLL_NAME = "vinted"
@@ -8,19 +8,30 @@ MONGODB_NAME = "vinted"
 class MongoDB:
     def __init__(self, conn_string=MONGODB_CONNSTRING, db_name=MONGODB_NAME,
                  collection_name=COLL_NAME):
-        self._conn = MongoClient(conn_string)
+        self._conn = motor.AsyncIOMotorClient(conn_string)
         self._db = self._conn[db_name]
         self._collection = self._db[collection_name]
 
-    def insert(self, data):
-        return self._collection.insert_one(data)
+    async def insert(self, item):
+        """Inserts an item to the database
 
-    def delete(self, data):
-        return self._collection.delete_one(data)
+        Keyword arguments:
+        item -- the item to insert
+        """
+        await self._collection.insert_one(item)
 
-    def print_all(self):
-        for doc in self._collection.find():
-            print(doc)
+    async def find(self, query):
+        """Finds items in the database
 
-    def drop_collection(self):
-        self._collection.drop()
+        Keyword arguments:
+        query -- the query to search for
+        """
+        return self._collection.find(query)
+    
+    async def delete(self, query):
+        """Deletes items from the database
+
+        Keyword arguments:
+        query -- the query to search for
+        """
+        return self._collection.delete_many(query)
